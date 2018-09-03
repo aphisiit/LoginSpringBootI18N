48b7a4d7d1916a86b309f8971aebd98ee2ebf9c9
Aphisit Namracha <aphisiit086757@hotmail.com>
2018-09-03 15:42:07 +0700

    Add Specification for search findOne().

--- src/main/java/com/guy/login/repository/TaskRepository.java
+++ src/main/java/com/guy/login/repository/TaskRepository.java
@@ -3,7 +3,8 @@ package com.guy.login.repository;
 import com.guy.login.domain.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.CrudRepository;
 
-public interface TaskRepository extends JpaSpecificationExecutor<Task>, JpaRepository<Task,Long> {
+public interface TaskRepository extends JpaSpecificationExecutor<Task>, JpaRepository<Task,Long>, CrudRepository<Task,Long> {
 
 }
